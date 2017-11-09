da523fdb6d6d5c35b10a401d74085099406e5f09
ajeeshameen <Nithin.Akkarapatty1@t-mobile.com>
2017-11-09 18:22:44 +0530

    correcting the script to create keypairs
    Former-commit-id: 2e8ca6c0bcd23797cb60513563174291339c7bcf

--- installscripts/terraform-unix-demo-jazz/scripts/createKeypair.sh
+++ installscripts/terraform-unix-demo-jazz/scripts/createKeypair.sh
@@ -2,7 +2,6 @@
 
 KEY_NAME=$1
 PRIVATE_KEY=$2
-
+sudo rm -rf $PRIVATE_KEY
 aws ec2 create-key-pair --key-name $KEY_NAME --query 'KeyMaterial' --output text > $PRIVATE_KEY
-chmod 400 $PRIVATE_KEY
-
+sudo chmod 400 $PRIVATE_KEY
